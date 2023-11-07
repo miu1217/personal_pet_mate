4f0555ce4e243d100edf36a3f6e0d4ee67837bfe
miu1217 <hyun671217@daum.net>
2023-11-07 16:05:12 +0900

    상품 삭제 기능입니다.

---
+++ src/main/java/com/kh/admin/controller/AdminProductDeleteController.java
@@ -0,0 +1,55 @@
+package com.kh.admin.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.kh.admin.model.service.AdminProductService;
+
+/**
+ * Servlet implementation class AdminProductDeleteController
+ */
+@WebServlet("/admin_delete.pd")
+public class AdminProductDeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminProductDeleteController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int productNo = Integer.parseInt(request.getParameter("pno"));
+		
+		int result = new AdminProductService().deleteProduct(productNo);
+		
+		HttpSession session = request.getSession();
+		if(result > 0) {
+			session.setAttribute("message", "상품 삭제 성공하셨습니다.");
+			response.sendRedirect(request.getContextPath() + "/admin_list.pd");
+		}else {
+			session.setAttribute("message", "상품 삭제 실패하셨습니다.");
+			response.sendRedirect(request.getContextPath() + "/admin_detail.pd?pno="+ productNo);
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
