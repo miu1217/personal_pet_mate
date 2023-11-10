fb1b92c06742ea23b50c0a1ca91a00e671fc87af
user1 <user1@DESKTOP-5RFL6US>
2023-11-10 12:20:20 +0900

    try catch로 ajax데이터 index보내주기

--- src/main/java/com/kh/product/controller/IndexProductController.java
+++ src/main/java/com/kh/product/controller/IndexProductController.java
@@ -2,16 +2,21 @@ package com.kh.product.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
+import com.kh.member.model.vo.Member;
 import com.kh.product.model.service.ProductService;
 import com.kh.product.model.vo.Product;
+import com.kh.product.model.vo.ProductCategory;
 
 /**
  * Servlet implementation class IndexProductController
@@ -33,11 +38,47 @@ public class IndexProductController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		ArrayList<Product> ipList = new ProductService().selectIndexProductList();
+		HttpSession session = request.getSession();
+		try {
+			
+			Member m = (Member)session.getAttribute("loginUser");
+			
+			ArrayList<String> mList = new ArrayList<>();
+			mList.add(m.getFoodInter());
+			mList.add(m.getCleanInter());
+			//상품리스트조회메소드
+			ArrayList<Product> ipList = new ProductService().selectIndexProductList();
+			
+			ArrayList<ProductCategory> pcList = new ProductService().selectProductCategoryList();
+			
+			Map<String, Object> dataMap = new HashMap<>();
+			dataMap.put("ipList", ipList);
+			dataMap.put("pcList", pcList);
+			dataMap.put("mList", mList);
+			
+			response.setContentType("json/appliaction; charset=UTF-8");
+			
+			new Gson().toJson(dataMap,response.getWriter());
+			
+		} catch(NullPointerException e) {
+			e.printStackTrace();
+			
+			ArrayList<Product> ipList = new ProductService().selectIndexProductList();
+			
+			ArrayList<ProductCategory> pcList = new ProductService().selectProductCategoryList();
+			ArrayList<String> mList = null;
+			
+			Map<String, Object> dataMap = new HashMap<>();
+			dataMap.put("ipList", ipList);
+			dataMap.put("pcList", pcList);
+			dataMap.put("mList", mList);
+			
+			response.setContentType("json/appliaction; charset=UTF-8");
+			
+			new Gson().toJson(dataMap,response.getWriter());
+		}
 		
-		response.setContentType("json/appliaction; charset=UTF-8");
 		
-		new Gson().toJson(ipList,response.getWriter());
 	}
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
