e488a0479ef7e9557a4f4dd1fc79ff64f4a9709d
user1 <user1@DESKTOP-5RFL6US>
2023-11-13 17:05:17 +0900

    필드에 parentCategoryNo 생성했습니다

--- src/main/java/com/kh/product/model/vo/Product.java
+++ src/main/java/com/kh/product/model/vo/Product.java
@@ -19,13 +19,25 @@ public class Product {
 	//사진게시판 목록에서 필요한 썸네일용 경로와 변경된파일이름(서버에업로드된 이름)을 합쳐 담을 필드변수
 	private String imgsrc;
 	
-	//타입비교용카테고리이름
+	//타입비교용카테고리이름,타입부모번호
 	private String categoryName;
+	private int parentCategoryNo;
 	
 	public Product() {
 		
 	}
 	
+	public Product(int productNo, String productName, String imgsrc, String categoryName, int parentCategoryNo) {
+		super();
+		this.productNo = productNo;
+		this.productName = productName;
+		this.imgsrc = imgsrc;
+		this.categoryName = categoryName;
+		this.parentCategoryNo = parentCategoryNo;
+	}
+
+
+
 	public Product(int productNo, String productName, String imgsrc, String categoryName) {
 		super();
 		this.productNo = productNo;
